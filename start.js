7175e97c302170865a370f9969e4174956742bbf
HxcoderIDOfc <149635285+HxcoderIDOfc@users.noreply.github.com>
2026-08-21 19:53:55 +0700

    chore: switch default session path to Axynera

--- start.js
+++ start.js
@@ -2,8 +2,8 @@ import fs from "node:fs";
 
 if (!process.env.WA_SESSION_DIR) {
   process.env.WA_SESSION_DIR = fs.existsSync("/data")
-    ? "/data/nextura-wa-session"
-    : "./data/wa-session";
+    ? "/data/axynera-wa-session"
+    : "./data/axynera-wa-session";
 }
 
 await import("./sdk-compat.js");
